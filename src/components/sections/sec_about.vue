644f10ed85e035622942b20060a97d8cd08b6e94
iEPCBM <rishatk87@gmail.com>
2021-09-19 16:53:33 +0300

    We are loose. I'm disapointed :(. Update

--- src/components/sections/sec_about.vue
+++ src/components/sections/sec_about.vue
@@ -4,7 +4,7 @@
     <div class="about-content">
       <img class="img-museum" src="../../assets/museum1.jpg" alt="">
       <div class="desc-wrapper">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="desc-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </div>
   </div>
@@ -35,7 +35,7 @@ export default {
 .desc-wrapper {
   align-self: flex-start;
 }
-.desc-wrapper>p {
+.desc-content {
   margin: 2em 1.5em;
   padding: 0;
 }
